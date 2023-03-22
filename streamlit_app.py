f4bcd5d51200fe4e3d3cb8c6256d906cc869e40e
luisneumann <luis.neumann@gmail.com>
2023-03-23 01:03:21 +1100

    Add second fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+second_fruit_choice = streamlit.text_input('What other fruit would you like information about?','Watermelon')
+streamlit.write('The user entered ', second_fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + f"{fruit_choice}")
 
