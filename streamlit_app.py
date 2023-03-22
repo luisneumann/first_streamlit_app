799db0463d89b110e2c5f195ebec6ec722c56ff0
luisneumann <luis.neumann@gmail.com>
2023-03-23 01:14:26 +1100

    add connection to send data to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-second_fruit_choice = streamlit.text_input('What other fruit would you like information about?','Watermelon')
-streamlit.write('The user entered ', second_fruit_choice)
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + f"{fruit_choice}")
 
@@ -41,3 +40,9 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
+
+
+add_my_fruit = streamlit.text_input('What  fruit would you like to add','Watermelon')
+streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding', fruit_choice)
+my_cur.execute('insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ("from streamlit");')
