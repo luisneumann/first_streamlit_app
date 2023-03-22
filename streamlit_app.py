c23a083675fb3f17396637f826336847d0535dbf
luisneumann <luis.neumann@gmail.com>
2023-03-23 01:15:49 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What  fruit would you like to add','Watermelon')
 streamlit.write('The user entered ', add_my_fruit)
-streamlit.write('Thanks for adding', fruit_choice)
-my_cur.execute('insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ("from streamlit");')
+streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute('insert into FRUIT_LOAD_LIST values ("from streamlit");')
